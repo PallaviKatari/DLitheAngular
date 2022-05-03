bd4372e9e0e3855114d80cf5a6c38b9c21c0ef3a
PallaviKatari <pallavikatari@gmail.com>
2022-05-03 18:10:24 +0530

    Commit_03.05.2022

--- src/app/customers/Customers.component.ts
+++ src/app/customers/Customers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CustomerService } from '../Customer.service';
 @Component({
   selector: 'app-customers',
@@ -7,28 +7,28 @@ import { CustomerService } from '../Customer.service';
 })
 export class CustomersComponent implements OnInit {
   users: any[];
+  //Accessing the service using constructor injection
+  //_customerService is an instance of the CustomerService class in Customer.service.ts
   constructor(private _customerService: CustomerService) {
-      this.users = this._customerService.getCustomer();
+    this.users = this._customerService.getCustomer();
   }
-
-  @Input()user!:any;
-
-userDetail=false;
-showUserDetailsToggle() {
-  this.userDetail = !this.userDetail;
-}
-getClass(){
-  var classList='';
-  if(this.userDetail){
-     classList = 'fa fa-plus'; 
-  }else {
+// import { Component, OnInit, Input } from '@angular/core';
+  @Input() user!: any;
+//Toggle effect to show and hide the customer details
+  userDetail = false;
+  showUserDetailsToggle() {
+    this.userDetail = !this.userDetail;
+  }
+  //class to show the change in + and - in the button
+  getClass() {
+    var classList = '';
+    if (this.userDetail) {
+      classList = 'fa fa-plus';
+    } else {
       classList = 'fa fa-minus';
+    }
+    return classList;
   }
-  return classList;
-}
-  
   ngOnInit(): void {
   }
-
-
 }
